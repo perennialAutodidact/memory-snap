e35c2ae1839e32e8094ddacd3a8d3dcdd14539f9
Keegan Good <perennialautodidact@gmail.com>
2023-08-30 20:18:28 -0700

    removed photos property

--- src/contexts/game/index.js
+++ src/contexts/game/index.js
@@ -1,21 +1,20 @@
-import { createContext } from "react";
+import { createContext } from 'react';
 
 export const baseState = {
   setup: {
     currentStep: 1,
     totalSteps: 3,
-    photos: [],
   },
   game: {
-    stage: "playing", // setup, playing, complete
+    stage: 'playing', // setup, playing, complete
     currentPlayer: 1,
     players: {
       1: {
-        name: "Player 1",
+        name: 'Player 1',
         score: 0,
       },
       2: {
-        name: "Player 2",
+        name: 'Player 2',
         score: 0,
       },
     },
