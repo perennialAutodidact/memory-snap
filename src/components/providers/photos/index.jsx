c36dc876a02292858db181efde88cd5b1781f91a
Keegan Good <perennialautodidact@gmail.com>
2023-08-31 01:42:48 -0700

    added logic to fetch photos and set photos context

--- src/components/providers/photos/index.jsx
+++ src/components/providers/photos/index.jsx
@@ -1,12 +1,23 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import PropTypes from 'prop-types';
 import { PhotosContext } from 'contexts/photos';
 import { baseState } from 'contexts';
 import { photosReducer } from 'contexts/photos/reducer';
+import useFetchedPhotos from 'hooks/useFetchedPhotos';
+import { setPhotos } from 'contexts/photos/actions';
 
 const PhotosProvider = ({ children, providedState = null } = {}) => {
   const initialState = providedState || baseState.photos;
   const [state, dispatch] = useReducer(photosReducer, initialState);
+
+  // TODO: add 'loading' value to indicate if the photos have loaded from the api
+  const { photos } = useFetchedPhotos({ query: 'cats', perPage: 5 });
+
+  useEffect(() => {
+    if (!photos) return;
+
+    dispatch(setPhotos(photos));
+  }, [photos]);
 
   return (
     <PhotosContext.Provider value={{ state, dispatch }}>
