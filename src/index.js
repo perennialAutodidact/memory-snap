6fd8ccd844590f3398d6fcb17726b1804c081cba
Keegan Good <perennialautodidact@gmail.com>
2023-08-31 01:39:39 -0700

    wrapped App in PhotosProvider

--- src/index.js
+++ src/index.js
@@ -6,13 +6,16 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import GameProvider from 'components/providers/game';
+import PhotosProvider from 'components/providers/photos';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <GameProvider>
-        <App />
+        <PhotosProvider>
+          <App />
+        </PhotosProvider>
       </GameProvider>
     </BrowserRouter>
   </React.StrictMode>
