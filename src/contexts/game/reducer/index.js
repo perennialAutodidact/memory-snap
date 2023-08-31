460ccacbfb42b043d89d456b82adf0e1057cf810
Keegan Good <perennialautodidact@gmail.com>
2023-08-31 01:42:15 -0700

    made error message more generic

--- src/contexts/game/reducer/index.js
+++ src/contexts/game/reducer/index.js
@@ -1,6 +1,6 @@
 export const gameReducer = (state, action) => {
   if (!state) {
-    throw new Error('please include a state object with a "photos" property');
+    throw new Error('please include a state object');
   } else if (!action || (action && !action.type)) {
     throw new Error('please include an action object with "type" property');
   }
