d42c430dc9b6289c779dc328f44619f8d1c8aa62
Keegan Good <perennialautodidact@gmail.com>
2023-08-30 19:51:00 -0700

    updated tests

--- src/contexts/game/actions/index.test.js
+++ src/contexts/game/actions/index.test.js
@@ -1,13 +1,17 @@
-import { setPhotos } from ".";
-import { mockPhotos } from "__mocks__/mockPhotos";
+import { setPhotos } from '.';
+import { mockPhotos } from 'components/__mocks__/mockPhotos';
+import types from './types';
 
-describe("GameContext action creator functions", () => {
-  describe("setPhotos", () => {
-    it("returns an action object with an array of photos", () => {
-      expect(setPhotos());
+describe('GameContext action creator functions', () => {
+  describe('setPhotos', () => {
+    it('returns an action object with an array of photos', () => {
+      expect(setPhotos(mockPhotos)).toEqual({
+        type: types.SET_PHOTOS,
+        payload: { photos: mockPhotos },
+      });
     });
 
-    it("throws an error if no photos array is passed", () => {
+    it('throws an error if no photos array is passed', () => {
       expect(() => setPhotos()).toThrow();
     });
   });
