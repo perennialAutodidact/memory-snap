aece4208501d8df8779c7267df06a3838f19a3f7
Keegan Good <perennialautodidact@gmail.com>
2023-08-31 01:41:54 -0700

    added reducer case for setting photos array

--- src/contexts/photos/reducer/index.js
+++ src/contexts/photos/reducer/index.js
@@ -1,11 +1,19 @@
+import types from '../actions/types';
+
 export const photosReducer = (state, action) => {
-  if (!action || (action && !action.type)) {
+  if (!state || (state && state.photos === undefined)) {
+    throw new Error('please include a state object');
+  } else if (!action || (action && !action.type)) {
     throw new Error('please include an action object with "type" property');
-  } else if (!state || (state && state.photos === undefined)) {
-    throw new Error('please include a state object with a "photos" property');
   }
 
   switch (action.type) {
+    case types.SET_PHOTOS: {
+      return {
+        ...state,
+        photos: action.payload.photos,
+      };
+    }
     default: {
       return state;
     }
--- src/contexts/photos/reducer/index.test.js
+++ src/contexts/photos/reducer/index.test.js
@@ -1,7 +1,26 @@
+import types from '../actions/types';
 import { photosReducer } from '.';
 import { baseState } from 'contexts';
+import { mockPhotos } from 'components/__mocks__/mockPhotos';
 
 describe('photosReducer', () => {
+  describe(`when called with the ${types.SET_PHOTOS} type`, () => {
+    it('returns state object with updated photos array', () => {
+      const photos = mockPhotos.slice(0, 1);
+      const payload = { photos };
+      const action = {
+        type: types.SET_PHOTOS,
+        payload,
+      };
+
+      const state = baseState.photos;
+      expect(photosReducer(state, action)).toStrictEqual({
+        ...baseState.photos,
+        photos,
+      });
+    });
+  });
+
   it('returns the default state if action type is unknown', () => {
     const { photos: state } = baseState;
     const action = {
