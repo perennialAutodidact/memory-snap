dbfae304ec6fd11231748e3f4c56f536ac17b366
PerennialAutodicact <56051123+perennialAutodidact@users.noreply.github.com>
2023-08-31 01:45:52 -0700

    16 connect photosprovider (#17)
    * wrapped App in PhotosProvider
    
    * moved photos action types, actions and tets into photos dir
    
    * added reducer case for setting photos array
    
    * made error message more generic
    
    * added logic to fetch photos and set photos context
    
    * pull in photos from context instead of directly from hook

---
+++ src/contexts/photos/actions/index.test.js
@@ -0,0 +1,15 @@
+import { mockPhotos } from 'components/__mocks__/mockPhotos';
+import { setPhotos } from '.';
+import types from './types';
+
+describe('PhotosContext actions', () => {
+  describe('setPhotos', () => {
+    it(`returns an action object with type ${types.SET_PHOTOS} and a payload containing a photos array`, () => {
+      const photos = mockPhotos.slice(0, 1);
+      expect(setPhotos(photos)).toStrictEqual({
+        type: types.SET_PHOTOS,
+        payload: { photos },
+      });
+    });
+  });
+});
