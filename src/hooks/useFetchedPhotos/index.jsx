587cd0582a87fa733196a00d61212c861a757f9c
Keegan Good <perennialautodidact@gmail.com>
2023-08-31 00:45:29 -0700

    removed unused import

--- src/hooks/useFetchedPhotos/index.jsx
+++ src/hooks/useFetchedPhotos/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { createClient } from 'pexels';
 
 const useFetchedPhotos = ({ query = 'nature', perPage }) => {
