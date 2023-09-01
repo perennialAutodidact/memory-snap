32c83c0f5d28f0e456cb98ad10daa8b3d8bd8b47
Keegan Good <perennialautodidact@gmail.com>
2023-08-31 20:03:41 -0700

    setup monorepo with nx

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -8,6 +8,10 @@ app.get('/photos', (req, res) => {
   res.send({ apiKey: process.env.PEXELS_API_KEY });
 });
 
-app.listen(port, () => {
-  console.log(`App is listening on port ${port}`);
+app.listen(port, (error) => {
+  if(error){
+    console.error(error)
+  } else { 
+    console.log(`App is listening on port ${port}`);
+  }
 });
