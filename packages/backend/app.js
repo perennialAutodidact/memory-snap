e28044b9d8cad6c5fca171dafee187bc47f9f3bc
Keegan Good <perennialautodidact@gmail.com>
2023-09-03 09:25:37 -0700

    fixed line endings

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,17 +1,22 @@
-const express = require('express');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 3001;
 
-require('dotenv').config();
+require("dotenv").config();
 
-app.get('/photos', (req, res) => {
+const corsOptions = { origin: "http://localhost:3000" };
+
+app.use(cors());
+
+app.get("/photos", (req, res) => {
   res.send({ apiKey: process.env.PEXELS_API_KEY });
 });
 
 app.listen(port, (error) => {
-  if(error){
-    console.error(error)
-  } else { 
+  if (error) {
+    console.error(error);
+  } else {
     console.log(`App is listening on port ${port}`);
   }
 });
