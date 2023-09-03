62151d3e8f3c30c98780b016ebb49b54cf8c78dc
Keegan Good <perennialautodidact@gmail.com>
2023-09-02 18:02:46 -0700

    created endpoint for fetching photos. setup babel and jest testing in node

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,17 +1,39 @@
-const express = require('express');
+import express from 'express';
+import { createClient } from 'pexels';
+import dotenv from 'dotenv';
+
 const app = express();
 const port = 3001;
 
-require('dotenv').config();
+dotenv.config({ path: '../../.env.local' });
 
-app.get('/photos', (req, res) => {
-  res.send({ apiKey: process.env.PEXELS_API_KEY });
-});
+app.use(express.json());
 
-app.listen(port, (error) => {
-  if(error){
-    console.error(error)
-  } else { 
-    console.log(`App is listening on port ${port}`);
+app.get('/', async (req, res) => {
+  try {
+    const pexelsClient = createClient(process.env.PEXELS_API_KEY);
+    const response = await pexelsClient.photos.search('cat');
+
+    if (!response.photos) {
+      res.status(400).json({
+        message: 'There was a problem fetching photos from the Pexels API',
+      });
+    } else {
+      res.status(200).json(response);
+    }
+  } catch ({ message }) {
+    res.status(500).json({ message });
   }
 });
+
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(port, (error) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(`App is listening on port ${port}`);
+    }
+  });
+}
+
+export default app;
---
+++ packages/backend/app.test.js
@@ -0,0 +1,58 @@
+import * as request from 'supertest';
+import app from './app';
+import { mockPhotos } from './__mocks__/mockPhotos';
+import dotenv from 'dotenv';
+import { createClient } from 'pexels';
+
+dotenv.config({ path: '../../.env.local' });
+
+describe('app', () => {
+  let server;
+  let agent;
+
+  beforeEach((done) => {
+    server = app.listen(4000, (error) => {
+      if (error) return done();
+
+      agent = request.agent(server);
+      done();
+    });
+  });
+
+  afterEach(async () => {
+    if (server) {
+      await server.close();
+    }
+    // jest.resetAllMocks();
+  });
+
+  it('returns photos array', async () => {
+    createClient.mockImplementationOnce(() => ({
+      photos: {
+        search: jest.fn().mockResolvedValue({ photos: mockPhotos }),
+      },
+    }));
+
+    await agent
+      .get('/')
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual({ photos: mockPhotos });
+      });
+  });
+
+  it('returns error on fail', async () => {
+    createClient.mockImplementationOnce(() => ({
+      photos: {
+        search: jest.fn().mockRejectedValueOnce({ message: 'oops!' }),
+      },
+    }));
+
+    await agent
+      .get('/')
+      .expect(500)
+      .then((response) => {
+        expect(response.body).toStrictEqual({ message: 'oops!' });
+      });
+  });
+});
