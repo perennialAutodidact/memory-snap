6bbbf067c2e7a01c711788ac82f6e5fdfc432e97
Keegan Good <perennialautodidact@gmail.com>
2023-09-04 13:19:36 -0700

    setup mock backend api for tests

--- packages/frontend/src/setupTests.js
+++ packages/frontend/src/setupTests.js
@@ -3,3 +3,12 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { mswServer } from '__mocks__/api';
+
+beforeEach(() => {
+  mswServer.listen();
+});
+afterEach(() => mswServer.resetHandlers());
+afterAll(() => {
+  mswServer.close();
+});
