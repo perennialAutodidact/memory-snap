0611abd7409adb7443108430bbf307e37c25c4ab
Keegan Good <perennialautodidact@gmail.com>
2023-09-04 13:15:39 -0700

    added cors and returned photos and other data

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,21 +1,35 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = 3001;
+const { mockPhotos } = require("./__mocks__/mockPhotos");
+
+const allowedOrigins = ["http://localhost:3000", "https://localhost:3000"];
+const corsOptions = {
+  origin: (origin, callback) => {
+    allowedOrigins.indexOf(origin) !== -1
+      ? callback(null, true)
+      : callback(new Error(`${origin} is not a trusted origin`));
+  },
+};
+app.use(cors(corsOptions));
+
+const port = process.env.API_PORT || 8080;
 
 require("dotenv").config();
 
-const corsOptions = { origin: "http://localhost:3000" };
-
-app.use(cors());
-
 app.get("/photos", (req, res) => {
-  res.send({ apiKey: process.env.PEXELS_API_KEY });
+  // TODO: add search params from req.query into image request on
+  //       branch 22-endpoint-for-returning-pexels-image-array
+
+  const { query, perPage } = req.query;
+  const apiKey = process.env.PEXELS_API_KEY;
+
+  res.status(200).json({ photos: mockPhotos, query, perPage, apiKey });
 });
 
 app.listen(port, (error) => {
   if (error) {
-    console.error(error);
+    throw new Error(error);
   } else {
     console.log(`App is listening on port ${port}`);
   }
