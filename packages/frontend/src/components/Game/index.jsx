890b8704ac117ce5c994e414f680247d5eb991e2
Keegan Good <perennialautodidact@gmail.com>
2023-09-07 16:26:03 -0700

    merge

--- packages/frontend/src/components/Game/index.jsx
+++ packages/frontend/src/components/Game/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import usePhotosContext from 'hooks/usePhotosContext';
 import Tile from './GameBoard/Tile';
+import PlayerHUD from './PlayerHUD';
+import useGameContext from 'hooks/useGameContext';
 
 const Game = () => {
   const {
     state: { photos },
   } = usePhotosContext();
 
+<<<<<<< Updated upstream
   console.log({ photos });
+=======
+  const {
+    state: { players, currentPlayer },
+  } = useGameContext();
+>>>>>>> Stashed changes
 
   return (
     <section aria-label="memory snap game">
+      <PlayerHUD player={players[currentPlayer]} />
       Game
       <Tile />
     </section>
