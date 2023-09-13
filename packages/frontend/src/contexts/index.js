23c15d3383ff5312a30b175826f34194c1e7b6a7
PerennialAutodicact <56051123+perennialAutodidact@users.noreply.github.com>
2023-09-13 13:18:56 -0700

    35 setup form context (#41)
    * setup form context, actions, reducer
    
    * baseState organization
    
    * updated tests
    
    * cleaned up logic
    
    * setup form context provider component and hook

--- packages/frontend/src/contexts/index.js
+++ packages/frontend/src/contexts/index.js
@@ -1,26 +1,9 @@
+import { initialFormState } from './form';
+import { initialGameState } from './game';
+import { initialPhotosState } from './photos';
+
 export const baseState = {
-  photos: {
-    photos: null,
-    loading: false,
-  },
-  setup: {
-    currentStep: 1,
-    totalSteps: 3,
-  },
-  game: {
-    stage: 'playing', // setup, playing, complete
-    currentPlayer: 1,
-    players: {
-      1: {
-        name: 'Player 1',
-        number: 1,
-        score: 0,
-      },
-      2: {
-        name: 'Player 2',
-        number: 2,
-        score: 0,
-      },
-    },
-  },
+  form: initialFormState,
+  game: initialGameState,
+  photos: initialPhotosState,
 };
