9df73bb40eca4b141fff7448e081a381d878292d
Russell Fraze <russell.fraze@gmail.com>
2023-09-13 11:27:08 -0700

    tile test started

---
+++ packages/frontend/src/components/Game/GameBoard/Tile/index.test.jsx
@@ -0,0 +1,9 @@
+import { setupTests } from 'helpers/tests';
+import Tile from '.';
+
+describe('Tile component', () => {
+  it('renders', () => {
+    const { screen } = setupTests(Tile);
+    expect(screen.getByLabelText('back')).toBeInTheDocument();
+  });
+});
