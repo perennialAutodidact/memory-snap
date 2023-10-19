36ad6effbcaa4d76d8f105803b8eb82ad0f02f66
Russell Fraze <russell.fraze@gmail.com>
2023-10-19 14:47:04 -0700

    game board parent of tile grid

--- packages/frontend/src/components/Game/GameBoard/index.jsx
+++ packages/frontend/src/components/Game/GameBoard/index.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
 // import PropTypes from "prop-types";
 // import Tile from "./Tile";
+import useGameContext from 'hooks/useGameContext';
+import ScoreBoard from '../ScoreBoard';
+import TileGrid from './TileGrid';
 
 const GameBoard = () => {
-  return <div></div>;
+  const {
+    state: { players, currentPlayer, tiles },
+  } = useGameContext();
+  console.log(tiles, 'TIles from GB');
+  return (
+    <>
+      <ScoreBoard players={players} currentPlayer={currentPlayer} />
+      <TileGrid tiles={tiles} />
+    </>
+  );
 };
 
 export default GameBoard;
