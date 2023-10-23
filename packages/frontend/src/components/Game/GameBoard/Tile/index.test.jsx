ee5945ae13fd12ea5356363883b6a7be8c8102bb
Russell Fraze <russell.fraze@gmail.com>
2023-10-23 11:24:00 -0700

    tile tests props change

--- packages/frontend/src/components/Game/GameBoard/Tile/index.test.jsx
+++ packages/frontend/src/components/Game/GameBoard/Tile/index.test.jsx
@@ -7,30 +7,28 @@ import userEvent from '@testing-library/user-event';
 describe('Tile component', () => {
   it('renders', () => {
     const tile = createTilesFromPhotos(mockPhotos)[0];
+    const props = { tile }
 
-    const { screen } = setupTests(Tile, { props: { tile: tile } });
+    const { screen } = setupTests(Tile, { props });
     expect(screen.getByTestId(/tile/)).toBeInTheDocument();
   });
 
   it('has alt text if faceUp is true', () => {
     const tile = createTilesFromPhotos(mockPhotos)[0];
-
-    const { screen } = setupTests(Tile, {
-      props: { tile: { ...tile, faceUp: true } },
-    });
+    const props = { tile: {...tile, faceUp: true}};
+    
+    const { screen } = setupTests(Tile, { props });
 
     expect(screen.getByAltText(tile.photo.alt)).toBeInTheDocument();
   });
 
   it('does not have alt text if faceUp is false', () => {
     const tile = createTilesFromPhotos(mockPhotos, { shuffle: true })[0];
-    const props = { ...tile, faceUp: false };
+    const props = { tile: { ...tile, faceUp: false} };
 
-    const { screen } = setupTests(Tile, {
-      props: { tile: { ...tile, faceUp: false } },
-    });
+    const { screen } = setupTests(Tile, { props });
 
-    const element = screen.queryByAltText(props.photo.alt);
+    const element = screen.queryByAltText(tile.photo.alt);
 
     expect(element).not.toBeInTheDocument();
   });
