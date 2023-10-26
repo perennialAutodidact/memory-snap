4a9294296d055aef65167fa7e6dfdefaee4072ca
Russell Fraze <russell.fraze@gmail.com>
2023-10-26 15:18:28 -0700

    test help

--- packages/frontend/src/components/Game/GameBoard/TileGrid/index.test.jsx
+++ packages/frontend/src/components/Game/GameBoard/TileGrid/index.test.jsx
@@ -2,6 +2,7 @@ import { setupTests } from 'helpers/tests';
 import { createTilesFromPhotos } from 'helpers/createTilesFromPhotos';
 import TileGrid from '.';
 import { mockPhotos } from '__mocks__/api/mockPhotos';
+import userEvent from '@testing-library/user-event';
 
 describe('TileGrid component', () => {
   it('renders all tiles', () => {
@@ -12,5 +13,25 @@ describe('TileGrid component', () => {
     const allTiles = screen.getAllByTestId(/tile/);
 
     expect(allTiles.length).toBe(10);
+  });
+
+  it('flips a tile after two seconds when onFlipTile is called', async () => {
+
+    //setup the TileGrid component with tiles prop
+    const tiles = createTilesFromPhotos(mockPhotos);
+
+    const { screen } = setupTests(TileGrid, { props: { tiles } });
+    
+    
+    
+    const tile = screen.getByTestId('tile-1');
+    screen.debug(tile);
+    
+    const user = userEvent.setup();
+    await user.click(screen.getByTestId('tile-1'));
+
+
+    // expect(onFlipTile).toHaveBeenCalledTimes(1);
+
   });
 });
