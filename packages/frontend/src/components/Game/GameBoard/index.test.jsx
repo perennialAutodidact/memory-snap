0119154b2f6e56ecf3f4f252077a3882a07046ec
Russell Fraze <russell.fraze@gmail.com>
2023-11-01 11:39:25 -0700

    testing library selector added

--- packages/frontend/src/components/Game/GameBoard/index.test.jsx
+++ packages/frontend/src/components/Game/GameBoard/index.test.jsx
@@ -4,7 +4,8 @@ import { mockPhotos } from '__mocks__/api/mockPhotos';
 import { createTilesFromPhotos } from 'helpers/createTilesFromPhotos';
 import { baseState } from 'contexts';
 import { produce } from 'immer';
-import { waitFor, act } from '@testing-library/react';
+import { act } from '@testing-library/react';
+import { byTestId, byAltText } from 'testing-library-selector';
 
 beforeEach(() => {
   jest.useFakeTimers('legacy');
@@ -41,6 +42,13 @@ describe('GameBoard component', () => {
     expect(displayedImage).toBeInTheDocument();
   });
 
+  const ui = {
+    tile1: byTestId('tile-1'),
+    tile2: byTestId('tile-5'),
+    tileAlt1: byAltText(/Jellyfish/),
+    tileAlt2: byAltText(/Splashing/),
+  };
+
   it('unmatched tiles are flipped back after two seconds', async () => {
     const tiles = createTilesFromPhotos(mockPhotos, { shuffle: false });
 
@@ -49,25 +57,22 @@ describe('GameBoard component', () => {
     });
     const state = { ...baseState, game: initialGameState };
 
-    const { screen, user } = setupTests(GameBoard, { state });
+    const { user } = setupTests(GameBoard, { state });
 
-    const element1 = screen.getByTestId('tile-1');
-    const element2 = screen.getByTestId('tile-5');
+    expect(ui.tile1.get()).toHaveClass('faceDown');
 
-    expect(screen.getByTestId('tile-1')).toHaveClass('faceDown');
+    await user.click(ui.tile1.get());
+    await user.click(ui.tile2.get());
 
-    await user.click(element1);
-    await user.click(element2);
-
-    expect(screen.getByAltText(/Jellyfish/)).toBeInTheDocument();
-    expect(screen.getByAltText(/Splashing/)).toBeInTheDocument();
+    expect(ui.tileAlt1.get()).toBeInTheDocument();
+    expect(ui.tileAlt2.get()).toBeInTheDocument();
 
     act(() => jest.advanceTimersByTime(3000));
 
-    expect(screen.getByTestId('tile-1')).toHaveClass('faceDown');
-    expect(screen.getByTestId('tile-5')).toHaveClass('faceDown');
+    expect(ui.tile1.get()).toHaveClass('faceDown');
+    expect(ui.tile2.get()).toHaveClass('faceDown');
 
-    expect(screen.queryByAltText(/Jellyfish/)).toBeNull();
-    expect(screen.queryByAltText(/Splashing/)).toBeNull();
+    expect(ui.tileAlt1.query()).toBeNull();
+    expect(ui.tileAlt2.query()).toBeNull();
   });
 });
