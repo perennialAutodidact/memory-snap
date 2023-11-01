bf37729b3238772d1ec0c7ea47682a7b204af43a
Russell Fraze <russell.fraze@gmail.com>
2023-11-01 13:08:52 -0700

    pre-commit linting test

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
