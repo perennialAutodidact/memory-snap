d4af97d8c56bce50bc4161b2debd97db438ce090
Russell Fraze <russell.fraze@gmail.com>
2023-11-06 13:39:43 -0800

    tile tests added

--- packages/frontend/src/components/Game/GameBoard/Tile/index.test.jsx
+++ packages/frontend/src/components/Game/GameBoard/Tile/index.test.jsx
@@ -1,13 +1,15 @@
 import { setupTests } from 'helpers/tests';
+import { produce } from 'immer';
 import Tile from '.';
 import { mockPhotos } from '__mocks__/api/mockPhotos';
 import { createTilesFromPhotos } from 'helpers';
+import { baseState } from 'contexts';
 import userEvent from '@testing-library/user-event';
 
 describe('Tile component', () => {
   it('renders', () => {
     const tile = createTilesFromPhotos(mockPhotos)[0];
-    const props = { tile }
+    const props = { tile };
 
     const { screen } = setupTests(Tile, { props });
     expect(screen.getByTestId(/tile/)).toBeInTheDocument();
@@ -15,8 +17,8 @@ describe('Tile component', () => {
 
   it('has alt text if faceUp is true', () => {
     const tile = createTilesFromPhotos(mockPhotos)[0];
-    const props = { tile: {...tile, faceUp: true}};
-    
+    const props = { tile: { ...tile, faceUp: true } };
+
     const { screen } = setupTests(Tile, { props });
 
     expect(screen.getByAltText(tile.photo.alt)).toBeInTheDocument();
@@ -24,7 +26,7 @@ describe('Tile component', () => {
 
   it('does not have alt text if faceUp is false', () => {
     const tile = createTilesFromPhotos(mockPhotos, { shuffle: true })[0];
-    const props = { tile: { ...tile, faceUp: false} };
+    const props = { tile: { ...tile, faceUp: false } };
 
     const { screen } = setupTests(Tile, { props });
 
@@ -49,10 +51,47 @@ describe('Tile component', () => {
 
   it('has the class matched when isMatched is true', () => {
     const tile = createTilesFromPhotos(mockPhotos)[0];
-    const props = { tile: {...tile, isMatched: true}};
+    const props = { tile: { ...tile, isMatched: true } };
 
     const { screen } = setupTests(Tile, { props });
 
     expect(screen.getByTestId(`tile-${tile.id}`)).toHaveClass('matched');
-  })
+  });
+
+  it('is not clickable while face up', async () => {
+    const tile = createTilesFromPhotos(mockPhotos)[0];
+    const props = { tile: { ...tile, faceUp: true } };
+
+    const { screen } = setupTests(Tile, { props });
+
+    const user = userEvent.setup();
+
+    await user.click(screen.getByTestId(/tile/));
+
+    expect(screen.getByTestId(/tile/)).toHaveClass('faceUp');
+  });
+
+  it('is not clickable when two other tiles are face up', async () => {
+    const tiles = createTilesFromPhotos(mockPhotos, { shuffle: false });
+
+    const tile = tiles[0]
+
+    const flipped = [tiles[3], tiles[9]]
+
+    const twoFlippedGameState = produce(baseState.game, (draft) => {
+      draft.tiles = tiles;
+      draft.flipped = flipped;
+    });
+
+    const state = { ...baseState, game: twoFlippedGameState };
+
+    const onFlip = jest.fn();
+
+    const { screen, user } = setupTests(Tile, {props: {tile, onFlip} }, { state });
+
+    await user.click(screen.getByTestId(/tile/));
+    
+    expect(screen.getByTestId('tile-0')).not.toHaveClass('faceUp');
+    expect(screen.queryByAltText(tiles[0].photo.alt)).not.toBeInTheDocument()
+  });
 });
