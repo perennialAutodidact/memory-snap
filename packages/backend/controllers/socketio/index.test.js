b794da9cfe597007609771b33dbc9d19e528b46e
PerennialAutodicact <56051123+perennialAutodidact@users.noreply.github.com>
2023-11-10 14:09:48 -0800

    43 setup sockets (#80)
    * started setting up sockets
    
    * more socket setup
    
    * created game and photos controllers
    
    * sockets setup stuff
    
    * socket test setup stuff
    
    * setup absolute imports
    
    * setup test socket
    
    * removed unnecessary comment
    
    * changed console.log to console.warn
    
    * added more specific eslint comment

---
+++ packages/backend/controllers/socketio/index.test.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import { setupTestSocket, waitFor } from '__test__/helpers';
+
+dotenv.config({ path: '.env.local' });
+
+describe('socket.io', () => {
+  let clientSocket, httpServer, io, serverSocket;
+  beforeEach(async () => {
+    const testSocket = await setupTestSocket();
+
+    httpServer = testSocket.httpServer;
+    io = testSocket.io;
+    serverSocket = testSocket.serverSocket;
+    clientSocket = testSocket.clientSocket;
+  });
+
+  afterEach(async () => {
+    serverSocket.disconnect();
+    clientSocket.disconnect();
+    await httpServer.close();
+    io.close();
+  });
+
+  it('responds to ping', async () => {
+    clientSocket.emit('ping');
+    const response = await waitFor(clientSocket, 'ping');
+    expect(response.message).toBe('pong');
+  });
+});
