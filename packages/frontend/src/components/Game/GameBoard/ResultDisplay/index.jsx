455f203774d2d166378f7191609237180bea05c8
Russell Fraze <russell.fraze@gmail.com>
2024-01-18 10:02:43 -0800

    result display component

---
+++ packages/frontend/src/components/Game/GameBoard/ResultDisplay/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import useGameContext from 'hooks/useGameContext';
+
+const ResultDisplay = () => {
+  const { state } = useGameContext();
+
+  let highScore = 0;
+
+  state.players.forEach((player) =>
+    player.score > highScore ? (highScore = player.score) : ''
+  );
+
+  //use map for this instead
+  let highScoreArray = [];
+  state.players.forEach((player) =>
+    player.score === highScore ? highScoreArray.push(player) : ''
+  );
+
+  return (
+    <div className="container d-flex justify-content-center mt-5">
+      <div className="d-flex-column text-center">
+        <h1 className="mb-5">GAME OVER!</h1>
+
+        {highScoreArray.length > 1 ? (
+          <h2 className="lh-lg">Its a tie!</h2>
+        ) : (
+          <h2 className={`text-${highScoreArray[0].color.className}`}>
+            {`${highScoreArray[0].name} wins!`}
+          </h2>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ResultDisplay;
