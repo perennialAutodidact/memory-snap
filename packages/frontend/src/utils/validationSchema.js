2712ed4c484cbfdb19f87dbf330cb95b495c2c75
Russell Fraze <russell.fraze@gmail.com>
2024-05-09 10:01:28 -0700

    validation schema defined

--- packages/frontend/src/utils/validationSchema.js
+++ packages/frontend/src/utils/validationSchema.js
@@ -1,5 +1,14 @@
 import * as yup from 'yup';
 
 const schema = yup.object().shape({
-  playerOneName: yup.string().min(1).max(30).required(),
+  playerNames: yup
+    .array()
+    .of(yup.string().required().min(1).max(30))
+    .required()
+    .min(2)
+    .max(2),
+  numberOfTiles: yup.number().required().min(2).max(16),
+  photoQuery: yup.string().required().min(2).max(30),
 });
+
+export default schema;
