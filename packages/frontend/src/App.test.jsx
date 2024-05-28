aca5cf666860d8e409545621df06fb8eca18a506
Russell Fraze <russell.fraze@gmail.com>
2024-05-28 13:53:25 -0700

    tests moved from app to form

--- packages/frontend/src/App.test.jsx
+++ packages/frontend/src/App.test.jsx
@@ -16,7 +16,7 @@ describe('App', () => {
   it('it renders the setup component at /setup', () => {
     const setupTests = createSetupTestsForRoute('/setup');
 
-    const setupGameState = produce(baseState.game, (draft) => {
+    const setupGameState = produce(baseState.game, draft => {
       draft.stage = GAME_STAGES.SETUP;
     });
 
@@ -29,90 +29,10 @@ describe('App', () => {
     ).toBeInTheDocument();
   });
 
-  it('it renders the correct form component at /setup/step-1', () => {
-    const setupTests = createSetupTestsForRoute('/setup/step-1');
-
-    const setupGameState = produce(baseState.game, (draft) => {
-      draft.stage = GAME_STAGES.SETUP;
-    });
-
-    const setupFormState = produce(baseState.form, (draft) => {
-      draft.currentStep = 1;
-    });
-
-    const state = { ...baseState, game: setupGameState, form: setupFormState };
-
-    const { screen } = setupTests(App, { state });
-
-    const stepOneComponent = screen.getByText('Player 1 Name');
-
-    expect(stepOneComponent).toBeInTheDocument();
-  });
-
-  it('it renders the correct form component at /setup/step-2', () => {
-    const setupTests = createSetupTestsForRoute('/setup/step-2');
-
-    const setupGameState = produce(baseState.game, (draft) => {
-      draft.stage = GAME_STAGES.SETUP;
-    });
-
-    const setupFormState = produce(baseState.form, (draft) => {
-      draft.currentStep = 2;
-    });
-
-    const state = { ...baseState, game: setupGameState, form: setupFormState };
-
-    const { screen } = setupTests(App, { state });
-
-    const stepTwoComponent = screen.getByText('Player 2 Name');
-
-    expect(stepTwoComponent).toBeInTheDocument();
-  });
-
-  it('it renders the correct form component at /setup/step-3', () => {
-    const setupTests = createSetupTestsForRoute('/setup/step-3');
-
-    const setupGameState = produce(baseState.game, (draft) => {
-      draft.stage = GAME_STAGES.SETUP;
-    });
-
-    const setupFormState = produce(baseState.form, (draft) => {
-      draft.currentStep = 3;
-    });
-
-    const state = { ...baseState, game: setupGameState, form: setupFormState };
-
-    const { screen } = setupTests(App, { state });
-
-    const stepThreeComponent = screen.getByText('Number of Tiles');
-
-    expect(stepThreeComponent).toBeInTheDocument();
-  });
-
-  it('it renders the correct form component at /setup/step-4', () => {
-    const setupTests = createSetupTestsForRoute('/setup/step-4');
-
-    const setupGameState = produce(baseState.game, (draft) => {
-      draft.stage = GAME_STAGES.SETUP;
-    });
-
-    const setupFormState = produce(baseState.form, (draft) => {
-      draft.currentStep = 4;
-    });
-
-    const state = { ...baseState, game: setupGameState, form: setupFormState };
-
-    const { screen } = setupTests(App, { state });
-
-    const stepFourComponent = screen.getByText('Pictures Of');
-
-    expect(stepFourComponent).toBeInTheDocument();
-  });
-
   it('it renders the result display component at /game-over', () => {
     const setupTests = createSetupTestsForRoute('/game-over');
 
-    const gameOverState = produce(baseState.game, (draft) => {
+    const gameOverState = produce(baseState.game, draft => {
       draft.stage = GAME_STAGES.GAME_OVER;
       draft.winner = baseState.game.players[0];
     });
--- packages/frontend/src/components/Game/GameBoard/index.jsx
+++ packages/frontend/src/components/Game/GameBoard/index.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import useGameContext from 'hooks/useGameContext';
+import useFormContext from 'hooks/useFormContext';
 import ScoreBoard from '../ScoreBoard';
 import TileGrid from './TileGrid';
 
 const GameBoard = () => {
   const {
-    state: { players, currentPlayer, tiles },
+    state: { currentPlayer, tiles },
   } = useGameContext();
+
+  const formData = useFormContext();
 
   return (
     <>
-      <ScoreBoard players={players} currentPlayer={currentPlayer} />
+      <ScoreBoard
+        players={formData.state.formValues.playerNames}
+        currentPlayer={currentPlayer}
+      />
       <TileGrid tiles={tiles} />
     </>
   );
