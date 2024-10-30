0e310aca5441b8e5b5b10a087d4d30457d7f25f3
Russell Fraze <russell.fraze@gmail.com>
2024-10-29 19:02:39 -0700

    fix errors repeat

--- packages/frontend/src/components/Setup/FormStep/Input.jsx
+++ packages/frontend/src/components/Setup/FormStep/Input.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import useFormContext from 'hooks/useFormContext';
 
 const Input = ({ register, name, errors, placeholder }) => {
-  const {
-    state: { currentStep },
-  } = useFormContext();
   return (
     <>
       <input
@@ -14,11 +10,7 @@ const Input = ({ register, name, errors, placeholder }) => {
         placeholder={placeholder}
         {...register(name)}
       ></input>
-      <p className="align-self-start mt-3">
-        {errors[name]?.message ||
-          errors[name]?.message ||
-          errors[name]?.message}
-      </p>
+      <p className="align-self-start mt-3">{errors[name]?.message}</p>
     </>
   );
 };
@@ -26,6 +18,7 @@ Input.propTypes = {
   register: PropTypes.func,
   name: PropTypes.string,
   errors: PropTypes.object,
+  placeholder: PropTypes.string,
 };
 
 export default Input;
